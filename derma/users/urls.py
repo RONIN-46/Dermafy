cc8f718afe4d21c19e2358fbb862ca4b84a19bd5
pras2005 <prasaddesale2005@gmail.com>
2025-02-24 17:34:38 +0000

    login stuff done by prayag

---
+++ derma/users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path,include
+from .views import *
+
+urlpatterns =[
+        path('',home,name='home'),
+        path('user_login/',user_login, name='user_login'),
+        path('user_logout/',user_logout,name='user_logout'),
+        ]
--- derma/users/views.py
+++ derma/users/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.http import HttpResponse
+from django.contrib.auth import logout, login
 
 # Create your views here.
+
+def home(request):
+    return render(request, "index.html")
+
+def user_login(request):
+    if request.method=="POST":
+        username=request.POST["username"]
+        password=request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid username or password")
+
+    return render(request, "login.html")
+
+
+def user_logout(request):
+    logout(request)
+    return redirect("login") #Redirect to login page
+
+#def signup(request):
