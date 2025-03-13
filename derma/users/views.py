1875305c0971ae5216c4d13d4695895b76c58427
pras2005 <prasaddesale2005@gmail.com>
2025-03-13 17:16:47 +0000

    hell conqurered

--- derma/users/views.py
+++ derma/users/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login, logout, get_backends
+from django.contrib.auth import authenticate, login, logout, get_backends
 from .models import *
 from django.shortcuts import render, redirect,get_object_or_404
 from django.contrib import messages
